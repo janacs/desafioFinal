1ce385bc635811923821410b58712173f4acf3e2
Janderson <jandersonsouza_phn@hotmail.com>
2024-11-13 10:11:53 -0300

    upgradeAssets

--- src/Components/Sobre/Sobre.jsx
+++ src/Components/Sobre/Sobre.jsx
@@ -1,6 +1,10 @@
+import loja from "../../assets/images/loja.png"
+
+
 export default function Sobre() {
     return(
         <section>
+            <img src={loja} alt="imagem da entrada da loja"/>
             <h2>Eu sou o Sobre</h2>
         </section>
     )
