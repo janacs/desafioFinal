3d3a1178401410d92be21b1d1c2eab54e3deb018
Janderson <jandersonsouza_phn@hotmail.com>
2024-11-18 14:56:22 -0300

    update do Sobre

--- src/Components/Sobre/Sobre.jsx
+++ src/Components/Sobre/Sobre.jsx
@@ -1,11 +1,20 @@
 import loja from "../../assets/images/loja.png"
+import { Section, Div, Button } from "./style";
 
-
-export default function Sobre() {
-    return(
-        <section>
-            <img src={loja} alt="imagem da entrada da loja"/>
-            <h2>Eu sou o Sobre</h2>
-        </section>
-    )
+export default function Sobre(){
+    return (
+      <Section>
+        <img src={loja} alt="" />
+        <Div>
+          <h3>PREPARAÇÃO</h3>
+          <h2>Níveis de torra</h2>
+          <p>
+            Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou
+            Torra Escura? Estas sãos as torras que fazem parte dos níveis de
+            torra Starbucks®
+          </p>
+          <Button>SAIBA MAIS</Button>
+        </Div>
+      </Section>
+    );
 }
---
+++ src/Components/Sobre/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 80vh;
+  color: #1e3932;
+`;
+
+export const Div = styled.div`
+width: 30vw;
+
+h2{
+    font-size: 40px;
+}
+`
+
+export const Button = styled.button`
+  background-color: #037143;
+  color: white;
+  border: none;
+  padding: 10px 30px;
+  font-weight: bold;
+  border-radius: 30px;
+`;
