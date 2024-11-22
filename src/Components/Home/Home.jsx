788b270501e6d87dc5fcfc503ce9b87f3846832d
Janderson <jandersonsouza_phn@hotmail.com>
2024-11-22 11:04:13 -0300

    updates da Home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,17 +1,29 @@
+import {MainStyle} from "./style";
+
 export default function() {
     return(
-        <main>
+        <MainStyle>
             <section>
             <h2>Mais que Café</h2>
             <h2>Isso é <span> Starbucks</span></h2>
             <p>A Starbucks oferece uma variedade de cafés de alta qualidade. Alguns dos cafés mais populares incluem o Caffè Americano, o Cappuccino, o Latte Macchiato e o Espresso. Além disso, a Starbucks oferece bebidas quentes e frias, doces diferenciados e sanduíches.</p>
             <button>SAIBA MAIS</button>
             <div>
-                <img src="https://github.com/janacs/desafioFinal/blob/main/src/assets/images/laranja.png?raw=true" alt="laranja" />
-                <img src="https://github.com/janacs/desafioFinal/blob/main/src/assets/images/vermelho.png?raw=true" alt="vermelho" />
-                <img src="https://github.com/janacs/desafioFinal/blob/main/src/assets/images/amarelo.png?raw=true" alt="amarelo" />
+                <img src="https://github.com/janacs/desafioFinal/blob/main/src/assets/images/copo%20laranja%20pequeno.png?raw=true" alt="laranja" />
+                <img src="https://github.com/janacs/desafioFinal/blob/main/src/assets/images/copo%20vermelho%20pequeno.png?raw=true" alt="vermelho" />
+                <img src="https://github.com/janacs/desafioFinal/blob/main/src/assets/images/copo%20amarelo%20pequeno.png?raw=true" alt="amarelo" />
             </div>
             </section>
-        </main>
-    )
+            <section>
+          <img
+            src="https://github.com/mariaccarolina/desafioSurpresa/blob/main/src/assets/images/laranja2x.png?raw=true"
+            alt=""
+          />
+          <img
+            src="https://github.com/mariaccarolina/desafioSurpresa/blob/main/src/assets/images/Ellipse%201.png?raw=true"
+            alt=""
+          />
+        </section>
+      </MainStyle>
+    );
 }
