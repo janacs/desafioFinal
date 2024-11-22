f3db4dae6526c5fdab8601a081ba1c8e9b2ee99b
Janderson <jandersonsouza_phn@hotmail.com>
2024-11-22 10:39:17 -0300

    update da Home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,17 @@
 export default function() {
     return(
         <main>
-            <h3>Sou a Home</h3>
+            <section>
+            <h2>Mais que Café</h2>
+            <h2>Isso é <span> Starbucks</span></h2>
+            <p>A Starbucks oferece uma variedade de cafés de alta qualidade. Alguns dos cafés mais populares incluem o Caffè Americano, o Cappuccino, o Latte Macchiato e o Espresso. Além disso, a Starbucks oferece bebidas quentes e frias, doces diferenciados e sanduíches.</p>
+            <button>SAIBA MAIS</button>
+            <div>
+                <img src="https://github.com/janacs/desafioFinal/blob/main/src/assets/images/laranja.png?raw=true" alt="laranja" />
+                <img src="https://github.com/janacs/desafioFinal/blob/main/src/assets/images/vermelho.png?raw=true" alt="vermelho" />
+                <img src="https://github.com/janacs/desafioFinal/blob/main/src/assets/images/amarelo.png?raw=true" alt="amarelo" />
+            </div>
+            </section>
         </main>
     )
 }
